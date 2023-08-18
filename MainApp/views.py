e8b926cfe628b877d29734fef145fb610b145212
Vadim <vadimx30@gmail.com>
2023-08-18 19:12:52 +0300

    Done M3_part1 practice 1-4

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,33 +1,13 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from MainApp.models import Item
+from django.core.exceptions import ObjectDoesNotExist
+
 
 # Create your views here.
 
-author = {
-    "Имя": "Иван",
-    "Отчество": "Петрович",
-    "Фамилия": "Иванов",
-    "телефон": "8-923-600-01-02",
-    "email": "vasya@mail.ru"
-
-}
-
-items = [
-   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
-   {"id": 5, "name": "Coca-cola 1 литр" ,"quantity":12},
-   {"id": 7, "name": "Картофель фри" ,"quantity":0},
-   {"id": 8, "name": "Кепка" ,"quantity":124},
-]
-
-
-
 
 def home(request):
-    # text = """<h1>"Изучаем django"</h1>
-    #           <strong>Автор</strong>: <i>Шиховцов В.В.</i>
-    #        """
-    # return HttpResponse(text)
     context = {
         "name": "Петров Иван Николаевич",
         "email": "my_mail@mail.ru"
@@ -36,6 +16,14 @@ def home(request):
 
 
 def about(request):
+    author = {
+    "Имя": "Иван",
+    "Отчество": "Петрович",
+    "Фамилия": "Иванов",
+    "телефон": "8-923-600-01-02",
+    "email": "vasya@mail.ru"
+
+    }
     result = f"""
     <header>
     /<a href="/">Home</a> / <a href="/items"> Items</a> / <a href="/about"> About</a>
@@ -55,16 +43,19 @@ def about(request):
 # url /item/2
 def get_item(request, item_id):
     """ По указанному id возвращает имя и количество"""
-    item = next((item for item in items if item['id'] == item_id), None)
-    if item:
+    try:
+        item = Item.objects.get(id=item_id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f'Item with id = {item_id} not found.')
+    else:
         context = {
             "item": item
         }
         return render(request, "item-page.html", context)
-    return HttpResponseNotFound(f'Item with id = {item_id} not found.')
-
+    
 
 def items_list(request):
+    items = Item.objects.all()
     context = {
         "items": items
     }
